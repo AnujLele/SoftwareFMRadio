f6d11107e6431ee06f5b007c8b3223da5909509b
AnujLele <leleanuj@gmail.com>
2024-04-27 13:11:21 -0400

    Final

--- FMRadio.py
+++ FMRadio.py
@@ -32,13 +32,14 @@ class FMRadio(QObject):
         open('/Users/anujlele/Documents/logFreq.txt', 'w').close() # Clear log file
         frequencies = [f for f in range(89,108,2)] # frequencies in MHz, spanning the FM spectrum
         duration = 3 # number of seconds per center frequency
-        candidates = []
+        candidates = [] 
 
         start_time = time.time()
         for freq in frequencies:
             self.GNURadio.set_centerFrequency(freq*(10**6))
             self.GNURadio.set_minBandwidth(86000)
             self.GNURadio.set_playAudio(False)
+            self.progressValue.emit(10)
             self.GNURadio.start()
 
             while (time.time() - start_time) < duration:
@@ -49,7 +50,7 @@ class FMRadio(QObject):
             self.GNURadio.stop()
             self.GNURadio.wait()
 
-            self.progressValue.emit(freq)
+            # self.progressValue.emit(freq)
 
         with open('/Users/anujlele/Documents/logFreq.txt', 'r') as file:
             for line in file:
